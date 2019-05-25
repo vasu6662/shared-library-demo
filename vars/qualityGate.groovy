39e7c56a25747f5e06f47dbf6d024606045328f0
Anoop Jain <anoop.jain10@gmail.com>
2019-05-26 01:40:14 +0530

    Update qualityGate.groovy

--- vars/qualityGate.groovy
+++ vars/qualityGate.groovy
@@ -1,6 +1,6 @@
 def call(def sonarqubeServer){
   withSonarQubeEnv("${sonarqubeServer}") {
-    timeout(time: 1, unit: 'HOURS') { 
+    timeout(time: 1, unit: 'MINUTES') { 
       def qg = waitForQualityGate() 
       if (qg.status != 'OK') {
         error "Pipeline aborted due to quality gate failure: ${qg.status}"
