52efccd6eb77e8f4908e7684f1ecc41a490e942a
Anoop Jain <anoop.jain10@gmail.com>
2019-05-26 00:40:41 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,10 +1,4 @@
-def call (def artifactoryServer, def mvnHome,def pom, def goal){
-  rtMavenDeployer (
-      id: 'deployer-unique-id',
-      serverId: "${artifactoryServer}",
-      releaseRepo: "generic-local/${BUILD_NUMBER}",
-      snapshotRepo: "generic-snapshot/${BUILD_NUMBER}"
-  )
+def call (def mvnHome,def pom, def goal){
 
   rtMavenRun (
       tool: "${mvnHome}",
