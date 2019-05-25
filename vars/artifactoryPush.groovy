ce7c36712a619aeb08cbc30d64f2e3d23dadfd72
Anoop Jain <anoop.jain10@gmail.com>
2019-05-26 01:01:09 +0530

    Delete artifactoryPush.groovy

--- vars/artifactoryPush.groovy
+++
@@ -1,8 +0,0 @@
-def call(def artifactoryServer, def releaseRepo, def snapshotRepo){
-   rtMavenDeployer (
-        id: 'deployer-unique-id',
-        serverId: "${artifactoryServer}",
-        releaseRepo: "${releaseRepo}/${BUILD_NUMBER}",
-        snapshotRepo: "${snapshotRepo}/${BUILD_NUMBER}"
-    )
-}
