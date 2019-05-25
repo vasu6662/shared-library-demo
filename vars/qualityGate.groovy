cf58527f4bf367d74b0304f0e2e8d6980b13923d
Anoop Jain <anoop.jain10@gmail.com>
2019-05-26 01:17:28 +0530

    Create qualityGate.groovy

---
+++ vars/qualityGate.groovy
@@ -0,0 +1,9 @@
+def call(){
+  timeout(time: 1, unit: 'HOURS') { 
+    def qg = waitForQualityGate() 
+    if (qg.status != 'OK') {
+      error "Pipeline aborted due to quality gate failure: ${qg.status}"
+      currentBuild.status='FAILURE'
+    }
+  }
+}
