75653c319e3e64b6a5c4d6fe8884b8773f365423
Anoop Jain <anoop.jain10@gmail.com>
2019-05-26 10:25:46 +0530

    Update runDockerImage.groovy

--- vars/runDockerImage.groovy
+++ vars/runDockerImage.groovy
@@ -1,3 +1,7 @@
 def call(def vmPort, def containerPort, def imageName, def tag){
+  def oldContainerTag = "${tag}-1"
+  echo "${oldContainerTag}"
+  sh "docker rm -f ${imageName}:${oldContainerTag}"
   sh " docker run -d -p ${vmPort}:${containerPort} ${imageName}:${tag}"
 }
+
