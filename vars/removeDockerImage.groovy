f8ceda559d365fd3bf33c7447f70d7d9906853b6
Anoop Jain <anoop.jain10@gmail.com>
2019-05-26 13:03:11 +0530

    Update removeDockerImage.groovy

--- vars/removeDockerImage.groovy
+++ vars/removeDockerImage.groovy
@@ -1,7 +1,7 @@
 def call(def dockerImageRemove ,def dockerImageName, def userName, def applicationName,def lastSuccessfulBuild){
   sh "docker rmi -f ${dockerImageRemove}/${dockerImageName}:${BUILD_NUMBER}"
   sh "docker rmi -f ${userName}/${applicationName}:${BUILD_NUMBER}"
-  sh "docker rm -f ${applicationName}-${lastSuccessfullBuild}"
+  sh "docker rm -f ${applicationName}-${lastSuccessfulBuild}"
   sh "docker rmi -f ${userName}/${applicationName}:${lastSuccessfulBuild}"
 }
 
