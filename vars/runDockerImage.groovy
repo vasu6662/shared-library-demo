49097d412a4b27177f1851e6261d4b38ba743f68
Anoop Jain <anoop.jain10@gmail.com>
2019-05-26 13:18:26 +0530

    Update runDockerImage.groovy

--- vars/runDockerImage.groovy
+++ vars/runDockerImage.groovy
@@ -1,5 +1,5 @@
 def call(def vmPort, def containerPort, def applicationName ,def imageName, def tag, def lastSuccessfullBuild){
-  sh " docker rm -f ${applicationName}-${lastSuccessfullBuild}"
+  //sh " docker rm -f ${applicationName}-${lastSuccessfullBuild}"
   sh " docker run -d -p ${vmPort}:${containerPort} --name=${applicationName}-${tag} ${imageName}:${tag}"
 }
 
